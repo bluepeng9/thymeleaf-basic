b329c3a56b8825904e017e301de057abb4ea6189
bluepeng9 <bluepeng9@gmail.com>
2023-05-13 16:44:54 +0900

    feat: Add example of text with unescaped

---
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -0,0 +1,28 @@
+package hello.thymeleaf.basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "<b>Hello Spring!</b>");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("text-unescaped")
+    public String textUnescaped(Model model) {
+        model.addAttribute("data", "<b>Hello Spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+}
